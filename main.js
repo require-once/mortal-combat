041e10dfd7783ce2a33ee3c9eb4af889ab5e701e
Дмитрий Жуков <d.zhukov@babyblog.team>
2021-10-14 18:10:12 +0300

    Task 1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const $player1 = {
+const player1 = {
     name: 'Sub-zero',
     hp: 100,
     img: '',
@@ -8,7 +8,7 @@ const $player1 = {
     }
 };
 
-const $player2 = {
+const player2 = {
     name: 'Liu-kang',
     hp: 100,
     img: '',
@@ -17,3 +17,31 @@ const $player2 = {
         console.log(this.name + ' ' + 'Fight...');
     }
 };
+
+function createPlayer() {
+    const $player1 = document.createElement('div');
+    $player1.classList.add('player1');
+
+    const $progressBar = document.createElement('div');
+    $progressBar.classList.add('progressbar');
+
+    const $life = document.createElement('div');
+    $life.classList.add('life');
+    $life.style.width = '100%';
+    $progressBar.appendChild($life);
+
+    const $name = document.createElement('div');
+    $name.classList.add('name');
+    $name.innerText = 'Sub-zero';
+    $progressBar.appendChild($name);
+
+    const $character = document.createElement('div');
+    $character.classList.add('character');
+
+    const $image = document.createElement('img');
+    $image.src = 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif';
+    $character.appendChild($image);
+
+    $player1.appendChild($progressBar);
+    $player1.appendChild($character);
+}
