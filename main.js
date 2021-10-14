595523524b24fdd233ec76336fb9726a86d710e7
Дмитрий Жуков <d.zhukov@babyblog.team>
2021-10-14 17:56:44 +0300

    Task 0

---
+++ main.js
@@ -0,0 +1,19 @@
+const $player1 = {
+    name: 'Sub-zero',
+    hp: 100,
+    img: '',
+    weapon: ['ice', 'snow'],
+    attack: function() {
+        console.log(this.name + ' ' + 'Fight...');
+    }
+};
+
+const $player2 = {
+    name: 'Liu-kang',
+    hp: 100,
+    img: '',
+    weapon: ['leg bicycle'],
+    attack: function() {
+        console.log(this.name + ' ' + 'Fight...');
+    }
+};
